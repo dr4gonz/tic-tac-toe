31490eec9b6d57a3fda2564aa7bcd63be9e6c1a8
Carl Egbert and Matthew Reyes <carl@>
2016-06-15 09:11:44 -0700

    add placePiece function to Game

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Global Variables
 var boardArray = [];
 var isGameActive = false;
-
+var turn;
 //Constructors
 Player = function(piece) {
   this.piece = piece;
@@ -11,6 +11,7 @@ Space = function(x, y) {
   this.x = x;
   this.y = y;
   this.isOccupied = false;
+  this.occupiedBy = "";
 }
 
 Piece = function(turn) {
@@ -20,13 +21,21 @@ Piece = function(turn) {
 
 Game = function() {
   isGameActive = true;
+  turn = 0;
   for (var iX = 0 ; iX < 3; iX++){
     for (var iY = 0 ; iY < 3; iY++){
       var gameSpace = new Space(iX,iY);
       boardArray.push(gameSpace);
     }
   }
+
 }
 //Game Logic
+Game.prototype.placePiece = function(x, y) {
+  boardArray[(y*3)+x].isOccupied = true;
+  var piece = new Piece(turn);
+  boardArray[(y*3)+x].occupiedBy = piece.type;
+  turn++;
+}
 
 //UI Logic
